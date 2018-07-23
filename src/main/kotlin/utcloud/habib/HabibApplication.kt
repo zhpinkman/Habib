6ae457f84943a9a7613e84dfebac83134eeb3bc5
Zhivar sourati <zh.sourati@ut.ac.ir>
2018-07-23 10:55:59 +0430

    initialized project added

---
+++ src/main/kotlin/utcloud/habib/HabibApplication.kt
@@ -0,0 +1,11 @@
+package utcloud.habib
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabibApplication
+
+fun main(args: Array<String>) {
+    runApplication<HabibApplication>(*args)
+}
